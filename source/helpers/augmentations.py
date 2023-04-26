d32bc41d4b3b679cd5c2719fb833cf592ab7b62e
Andrii Kapatsyn <prostoandrey01@gmail.com>
2023-04-26 13:52:24 +0300

    Changed augmentations

--- source/helpers/augmentations.py
+++ source/helpers/augmentations.py
@@ -5,21 +5,7 @@ transform = A.Compose(
     [
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.5),
-        A.ShiftScaleRotate(p=0.75),
         A.GridDistortion(num_steps=5, distort_limit=0.3, p=0.5),
-        A.CoarseDropout(
-            max_holes=1,
-            max_width=0.3,
-            max_height=0.3,
-            mask_fill_value=0,
-            p=0.5,
-        ),
-        A.OneOf(
-            [
-                A.GaussianBlur(),
-                A.MotionBlur(),
-            ],
-            p=0.4,
-        ),
+        A.ShiftScaleRotate(p=0.75),
     ]
 )
