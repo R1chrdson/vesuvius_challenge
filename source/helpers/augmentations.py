5b77d07a2ed1303ea8f3d84fd4cfd4af89731e47
Andrii Kapatsyn <prostoandrey01@gmail.com>
2023-04-27 13:34:35 +0300

    New augmentations v3

--- source/helpers/augmentations.py
+++ source/helpers/augmentations.py
@@ -5,7 +5,7 @@ transform = A.Compose(
     [
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.5),
-        A.GridDistortion(num_steps=5, distort_limit=0.3, p=0.5),
-        A.ShiftScaleRotate(p=0.75),
+        A.ShiftScaleRotate(p=0.75, rotate_limit=180, scale_limit=0.2, shift_limit=0.1),
+        A.GaussNoise(p=0.5, var_limit=5),
     ]
 )
